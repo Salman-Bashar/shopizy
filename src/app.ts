717f1f3450f619f62f9319796f78994f2cc9a378
Salman Bashar <salmanbadhon@gmail.com>
2021-08-06 20:21:50 +0600

    Added feature: Logger

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from "express"
 import config from "config"
+import log from "./logger"
 
 const app = express()
 
@@ -10,5 +11,5 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
 app.listen(port, host, () => {
-  console.log(`Server is running at http://${host}:${port}`)
+  log.info(`Server is running at http://${host}:${port}`)
 })
